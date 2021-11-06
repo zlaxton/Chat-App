46e7f81a10c13235c734ef62c09192f1546b9f9d
zlaxton <zlatan.kopic171@yahoo.com>
2021-11-06 22:09:55 +0100

    Update Chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,21 +1,86 @@
 import React from 'react';
-import { View, Text, Button, ImageBackground, TextInput, StyleSheet} from 'react-native';
+import { View, Platform, Text, Button, KeyboardAvoidingView, ImageBackground, TextInput, StyleSheet} from 'react-native';
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
+// Mapview Component
+import MapView from 'react-native-maps';
+//Async Storage
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+//NetInfo
+import NetInfo from '@react-native-community/netinfo';
 
 
 export default class Chat extends React.Component {
-  render() {
-    let name = this.props.route.params.name;
-    this.props.navigation.setOptions({ title: name });
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+    }
+  }
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+        {
+          _id: 2,
+          text: 'This is a system message',
+          createdAt: new Date(),
+          system: true,
+         },
+      ],
+    })
+  }
+  // Check if user is online or offline
+  NetInfo.fetch().then(connection => { 
+    if (connection.isConnected) {
+      this.setState({ isConnected: true });
+      console.log('online');
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: 'blue'
+          }
+        }}
+      />
+    )
+  }
+
+render() {
+  //let name = this.props.route.previousState.params.name;
+  //this.props.navigation.setOptions({ title: name });
 
     return (
-      <View style={{flex:1, justifyContent: 'center', alignItems: 'center', backgroundColor: "yellow"}}>
-        <Button
-        title="Go to Start"
-        onPress={() => this.props.navigation.navigate("Start")}
-        />
-        <Text>Hello Screen2!</Text>
+      <View style={{ flex: 1 }}>
+      <GiftedChat
+      renderBubble={this.renderBubble.bind(this)}
+      messages={this.state.messages}
+      onSend={messages => this.onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
+        {Platform.OS === "android" ? (
+          <KeyboardAvoidingView behavior="height" />
+        ) : null}
       </View>
-    )
+    );
   }
 }
 
