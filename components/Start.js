2431ee79c21697bc6c2956ad38eef4889bb14d5d
zlaxton <zlatan.kopic171@yahoo.com>
2022-01-05 14:13:01 +0100

    Update Start.js

--- components/Start.js
+++ components/Start.js
@@ -1,151 +1,232 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text,TextInput, ImageBackground, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import React, { Component } from "react";
+import {
+  Image,
+  View,
+  Text,
+  TextInput,
+  ImageBackground,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 
-// Set the Background Image
-const background = require('../images/pastel.jpg');
-// Background color options - Green, Blue, Purple, Gray, White
-const colors = ['#408337', '#3a5d84', '#78519d', '#606670', '#FFFFFF'];
-
-export default class Home extends React.Component {
+export default class Start extends React.Component {
   constructor(props) {
     super(props);
-    this.state ={
-      name: '',
-      backgroundColor: '#408337',
-    }
+    this.state = { name: "", colorSelectionBackground: "#757083" };
   }
 
-  // Check for username
-  onGoToChat = (name, backgroundColor) => {
-    if(name == '') {
-      return Alert.alert('Please enter your name.');
-    }
-    this.props.navigation.navigate('Chat', {
-      name: `${name}`,
-      backgroundColor: `${backgroundColor}`,
-    });
-  };
-
   render() {
-    const setColor = this.state.backgroundColor;
     return (
-      <View style={{flex:1, justifyContent: 'center'}}>
-        <ImageBackground source={background} style={styles.background}>
-          <View style={styles.titleWrapper}>
-            <Text style={styles.title}>Welcome!</Text>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <ImageBackground
+          style={styles.background}
+          source={require("../assets/background.jpg")} // Photo by Brooke Cagle on Unsplash
+        >
+          <View style={styles.titleView}>
+            <Text style={styles.title}>Chat App</Text>
           </View>
-          <View style={styles.wrapper}>
-          <TextInput 
-              style={styles.namefield}
-              onChangeText={(name) => this.setState({ name })}
-              value={this.state.name}
-              placeholder='Your Name'
-            />
-            <Text style={styles.text}>Select a Background Color:</Text>
-            <View style={styles.colorsMenu}>
-            {colors.map((selectedColor) => (
-              <TouchableOpacity
-              accessible={true}
-              accessibilityLabel="Select Background Color"
-              accessibilityHint="Sets your chat screens background color."
-              accessibilityRole="button"
-
-                key={selectedColor}
-                style={[
-                  styles.colorOptions(selectedColor),
-                  setColor === selectedColor ? styles.border : null,
-                ]}
-                activeOpacity={0.5}
-                onPress={() => this.setState({ backgroundColor: selectedColor })}
+          <View style={styles.whiteBox}>
+            <View style={styles.nameInputView}>
+              <TextInput
+                style={styles.nameInput}
+                onChangeText={(name) => this.setState({ name })}
+                value={this.state.name}
+                placeholder="Type your name..."
+              ></TextInput>
+              <Image
+                style={styles.icon}
+                source={require("../assets/icon.png")}
               />
-            ))}
-          </View>
+            </View>
+
+            <View style={styles.colorSelection}>
+              <Text style={styles.colorSelectionText}>
+                Choose Background Color:
+              </Text>
+              <View style={styles.colorSelectionPalette}>
+                <TouchableOpacity
+                  style={styles.colorSelectionPalette1}
+                  onPress={() =>
+                    this.setState({ colorSelectionBackground: "#f2bf5e" })
+                  }
+                  // Adds accessibility features
+                  accessible={true}
+                  accessibilityLabel="color: #f2bf5e"
+                  accessibilityHint="By clicking on this button, you can choose the chat room's background color."
+                  accessibilityRole="Button"
+                />
+                <TouchableOpacity
+                  style={styles.colorSelectionPalette2}
+                  onPress={() =>
+                    this.setState({ colorSelectionBackground: "#7da7d9" })
+                  }
+                  // Adds accessibility features
+                  accessible={true}
+                  accessibilityLabel="color: #7da7d9"
+                  accessibilityHint="By clicking on this button, you can choose the chat room's background color."
+                  accessibilityRole="Button"
+                />
+                <TouchableOpacity
+                  style={styles.colorSelectionPalette3}
+                  onPress={() =>
+                    this.setState({ colorSelectionBackground: "#b3e0f2" })
+                  }
+                  // Adds accessibility features
+                  accessible={true}
+                  accessibilityLabel="color: #b3e0f2"
+                  accessibilityHint="By clicking on this button, you can choose the chat room's background color."
+                  accessibilityRole="Button"
+                />
+                <TouchableOpacity
+                  style={styles.colorSelectionPalette4}
+                  onPress={() =>
+                    this.setState({ colorSelectionBackground: "#72b2a2" })
+                  }
+                  // Adds accessibility features
+                  accessible={true}
+                  accessibilityLabel="color: #72b2a2"
+                  accessibilityHint="By clicking on this button, you can choose the chat room's background color."
+                  accessibilityRole="Button"
+                />
+              </View>
+            </View>
             <TouchableOpacity
-              accessible={true}
-              accessibilityLabel="Go to chat"
-              accessibilityHint="Takes you to the chat screen."
-              accessibilityRole="button"
               style={styles.button}
-              onPress={() => this.onGoToChat(this.state.name, this.state.backgroundColor)}>  
-                <Text style={styles.text}>
-                  GO TO CHAT
-                </Text>
-              </TouchableOpacity>
+              onPress={() =>
+                this.props.navigation.navigate("Chat", {
+                  name: this.state.name,
+                  colorSelectionBackground: this.state.colorSelectionBackground,
+                })
+              }
+              // Adds accessibility features
+              accessible={true}
+              accessibilityLabel="Start Chatting"
+              accessibilityHint="By clicking on this button, you can enter the chat room."
+              accessibilityRole="Button"
+            >
+              <Text style={styles.buttonText}>Start Chatting</Text>
+            </TouchableOpacity>
           </View>
         </ImageBackground>
       </View>
     );
-  };
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
   background: {
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
     flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
+  },
 
-  },
-  titleWrapper: {
-    flex: 0.4,
-    justifyContent: 'space-evenly',
-  },
-  title: {
-    color: '#fff',
-    fontSize: 42,
-    fontWeight: 'bold',
-
-  },
-  text: {
-    color: "white",
-    fontSize: 16,
-  },
-  namefield: {
-    backgroundColor: '#fff',
-    width: '80%',
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    borderRadius: 3,
-    padding: 5,
-  },
-  wrapper: {
-    backgroundColor: 'rgba(52, 52, 52, 0.8)',
-    borderColor: '#ffa500',
-    borderWidth: 1,
-    width: '88%',
+  titleView: {
     flex: 0.6,
-    alignItems: 'center',
-    marginBottom: '8%',
-    flexDirection: 'column',
-    justifyContent: 'space-evenly',
   },
-  colorsMenu: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+
+  title: {
+    fontSize: 45,
+    fontWeight: "bold",
+    color: "#000",
+    height: 60,
   },
-  colorOptions: (selectedColor) => ({
-    backgroundColor: selectedColor,
-    width: 40,
-    height: 40,
-    marginHorizontal: 10,
-    borderWidth: 2,
-    borderRadius: 50,
-  }),
-  border: {
-    borderWidth: 2,
-    borderColor: '#ffa500',
+  whiteBox: {
+    flexDirection: "column",
+    justifyContent: "space-around",
+    backgroundColor: "#FFFFFF",
+    maxHeight: "44%",
+    minHeight: 260,
+    height: "44%",
+    width: "88%",
+    paddingLeft: "6%",
+    paddingRight: "6%",
+    top: "10%",
   },
+
+  colorSelection: {
+    height: 50,
+  },
+
+  nameInput: {
+    fontSize: 16,
+    fontWeight: "300",
+    color: "#757083",
+    opacity: 0.5,
+    borderWidth: 1,
+    borderColor: "#8a8697",
+    borderRadius: 2,
+    padding: 15,
+    paddingLeft: 50,
+  },
+
+  colorSelectionText: {
+    fontSize: 16,
+    fontWeight: "300",
+    color: "#757083",
+    bottom: 10,
+  },
+
+  colorSelectionPalette: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+
+  colorSelectionPalette1: {
+    backgroundColor: "#f2bf5e",
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+
+  colorSelectionPalette2: {
+    backgroundColor: "#7da7d9",
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+
+  colorSelectionPalette3: {
+    backgroundColor: "#b3e0f2",
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+
+  colorSelectionPalette4: {
+    backgroundColor: "#72b2a2",
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+
   button: {
-    width: '80%',
-    height: 40,
-    backgroundColor: '#757083',
-    color: '#FFFFFF',
-    alignItems: 'center',
-    fontWeight: 'bold',
-    justifyContent: 'space-evenly',
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 60,
+    backgroundColor: "#3089a2",
+  },
+
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+
+  icon: {
+    position: "absolute",
+    left: 0,
+    top: 4,
+    zIndex: -1,
+    width: 50,
+    height: 50,
   },
 });
