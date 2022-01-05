a236216e320754351735d453f9cc4f31c10e8284
zlaxton <zlatan.kopic171@yahoo.com>
2022-01-05 14:12:50 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,40 +1,22 @@
-//import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-//import { StyleSheet, Text, View } from 'react-native';
-import Chat from './components/Chat';
-import Start from './components/Start';
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-//import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React, { Component } from "react";
 
-//const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
+import Chat from "./components/Chat";
+import Start from "./components/Start";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default class App extends React.Component {
-  //constructor(props) {
-   // super(props);
-   // this.state = { text: ""};
- // }
-
-
+const Stack = createStackNavigator();
+export default class App extends Component {
   render() {
+    const Stack = createStackNavigator();
     return (
       <NavigationContainer>
-        <Tab.Navigator
-          initialRouteName="Start"
-        >
-          <Tab.Screen
-            name="Start"
-            component={Start}
-          />
-          <Tab.Screen
-            name="Chat"
-            component={Chat}
-          />
-        </Tab.Navigator>
+        <Stack.Navigator initialRouteName="Start">
+          <Stack.Screen name="Start" component={Start} />
+          <Stack.Screen name="Chat" component={Chat} />
+        </Stack.Navigator>
       </NavigationContainer>
-      
     );
-  }}
-
+  }
+}
